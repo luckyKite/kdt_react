3d94e50c771fe68322bc773911da8a16e2744ae7
채란 <nemo20435@gmail.com>
2023-01-19 16:33:14 +0900

    props 사용해서 전달하기

--- kdt_app/src/App.js
+++ kdt_app/src/App.js
@@ -1,8 +1,13 @@
+import Test from "./Test";
 
 function App() {
   return (
     <div>
-      <h1>GOOD!</h1>
+      <Test text="React"/>
+      <Test text="JSX" />
+      <Test text="어려워요?"/>
+      <Test />
+      <Test />
     </div>
   );
 }
---
+++ kdt_app/src/Test.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function Test(props) {
+  return ( 
+    <div>
+      <h1>{props.text} 여기에 값이 표현됩니다. </h1>
+    </div>
+  );
+}
+
