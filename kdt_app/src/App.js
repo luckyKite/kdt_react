ead11d642a3bca04ca69d55f23e50c961bfc5466
채란 <nemo20435@gmail.com>
2023-01-20 10:24:29 +0900

    starbucks 페이지

--- kdt_app/src/App.js
+++ kdt_app/src/App.js
@@ -1,22 +1,40 @@
-import {useState} from 'react';
-import Test from './Test';
+import { useState } from "react";
+import Lottie from 'react-lottie';
+
+import * as animationData from './lego.json';
+import Test from "./Test";
 
 function App() {
 
   const [number, setNumber] = useState(0);
 
+  const defaultOptions = {
+    loop: true,
+    autoplay: true, 
+    animationData: animationData,
+    rendererSettings: {
+      preserveAspectRatio: 'xMidYMid slice'
+    }
+  };
+
   const increment = () => {
-    setNumber(number+1);
+    setNumber(number + 1);
     console.log(number);
-  }
+  };
 
   const decrement = () => {
-    setNumber(number-1);
+    setNumber(number - 1);
     console.log(number);
-  }
+  };
 
   return (
     <div>
+      <div>
+        <Lottie options={defaultOptions}
+          height={400}
+          width={400}
+        />
+      </div>
       <h1>{number}</h1>
       <button onClick={increment}>Increment</button>
       <button onClick={decrement}>Decrement</button>
--- kdt_app/src/Test.jsx
+++ kdt_app/src/Test.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import InnerTest from './InnerTest';
 
-function Test({text}) {
+const Test = ({text}) => {
+  
   return ( 
     <div>
       <h1>{text} 값이 표현됩니다.</h1>
       <InnerTest innerTxt = {text}/>
-      <InnerTest innerTxt= "ABCD"/>
+      <InnerTest innerTxt = "ABCD"/>
     </div>
   );
 }
