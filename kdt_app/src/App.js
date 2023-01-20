4b3d736104170c24ae34a38c8070e5ecb1328a11
채란 <nemo20435@gmail.com>
2023-01-20 15:06:23 +0900

    import 오류 수정

--- kdt_app/src/App.js
+++ kdt_app/src/App.js
@@ -1,6 +1,6 @@
 import Footer from "./components/layouts/Footer";
 import Header from "./components/layouts/Header";
-import Main from "./components/pages/Main";
+import Main from "./pages/Main"
 
 function App() {
 
