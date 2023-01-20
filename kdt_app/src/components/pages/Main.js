ffba77ea9a68f005bc1d8a810aa863ed5f220c0a
채란 <nemo20435@gmail.com>
2023-01-20 13:30:36 +0900

    Update Main.js

--- kdt_app/src/components/pages/Main.js
+++ kdt_app/src/components/pages/Main.js
@@ -1,9 +1,15 @@
+import LineNotice from "../components/ui/main/LineNotice";
 import MainBanner from "../components/ui/main/MainBanner";
+import MainPromotionBanner from "../components/ui/main/MainPromotionBanner";
+import NewRewards from "../components/ui/main/NewRewards";
 
 function Main() {
   return ( 
     <div id="conatiner">
       <MainBanner/>
+      <LineNotice />
+      <MainPromotionBanner />
+      <NewRewards />
     </div>
   );
 }
