38914aa1eaa8cb8d6bab5debe951b07fde7e8037
채란 <nemo20435@gmail.com>
2023-01-20 14:49:58 +0900

    Create footerMenuDatas.js

---
+++ kdt_app/src/datas/footerMenuDatas.js
@@ -0,0 +1,119 @@
+const footerMenuDatas = [
+  {
+    id: 1,
+    name: "COMPANY",
+    link: "#",
+    subMenu: [
+      {
+        id: 1,
+        name: "한눈에 보기",
+        link: "/footer/company/index.do"
+      },
+      {
+        id: 2,
+        name: "스타벅스 사명",
+        link: "/footer/company/mission.do"
+      },
+      {
+        id: 3,
+        name: "스타벅스 소개",
+        link: "#"
+      },
+      {
+        id: 4,
+        name: "국내 뉴스룸",
+        link: "/footer/company/news_list.do"
+      },
+      {
+        id: 5,
+        name: "세계의 스타벅스",
+        link: "/footer/company/global_starbucks.do"
+      },
+      {
+        id: 6,
+        name: "글로벌 뉴스룸",
+        link: "https://news.starbucks.com"
+      }
+    ]
+  },
+  {
+    id: 2,
+    name: "CORPORATE SALES",
+    link: "#",
+    subMenu: [
+      {
+        id: 1,
+        name: "단체 및 기업 구매 안내",
+        link: "/footer/corporate_sales/index.do"
+      },
+      {
+        id: 2,
+        name: "단체 주문 배달 안내",
+        link: "/footer/corporate_sales/delivery.do"
+      }
+    ]
+  },
+  {
+    id: 3,
+    name: "PARTNERSHIP",
+    link: "#",
+    subMenu: [
+      { 
+        id: 1,
+        name: '신규 입점 제의',
+        link: '/footer/partnership/index.do'
+      },
+      {
+        id: 2,
+        name: '협력 고객사 등록신청',
+        link: '/footer/partnership/cooperation.do'
+      }
+    ]
+  },
+  {
+    id: 4,
+    name: "ONLINE COMMUNITY",
+    link: "#",
+    subMenu: [
+      {
+        id: 1,
+        name: '페이스북',
+        link: 'https://www.facebook.com/StarbucksKorea'
+      },
+      {
+        id: 2,
+        name: '인스타그램',
+        link: 'https://www.instagram.com/starbucks_korea/'
+      },
+      {
+        id: 3,
+        name: '유튜브',
+        link: 'https://www.youtube.com/user/StarbucksKorea'
+      },
+      {
+        id: 4,
+        name: '트위터',
+        link: 'https://twitter.com/StarbucksKorea'
+      }
+    ]
+  },
+  {
+    id: 5,
+    name: "RECRUIT",
+    link: "#",
+    subMenu: [
+      {
+        id: 1,
+        name: '채용 소개',
+        link: '/footer/recruit/index.do'
+      },
+      {
+        id: 2,
+        name: '채용 지원하기',
+        link: '/footer/recruit/apply.do'
+      }
+    ]
+  }
+];
+
+export default footerMenuDatas;
