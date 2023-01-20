cfe7bf6dbf47d6f70a8e39ec0f08c87a642cee13
채란 <nemo20435@gmail.com>
2023-01-20 14:05:58 +0900

    Create topMenuDatas.js

---
+++ kdt_app/src/datas/topMenuDatas.js
@@ -0,0 +1,29 @@
+const topMenuDatas = [
+  {
+    id: 1,
+    name: "Sign Out",
+    link: "#"
+  },
+  {
+    id: 2,
+    name: "Sign In",
+    link: "#"
+  },
+  {
+    id: 3,
+    name: "My Starbucks",
+    link: "/MyStarbucks"
+  },
+  {
+    id: 4,
+    name: "Customer Service & Ideas",
+    link: "/CustomerService"
+  },
+  {
+    id: 5,
+    name: "Find a Store",
+    link: "/FindAStore"
+  }
+];
+
+export default topMenuDatas;
