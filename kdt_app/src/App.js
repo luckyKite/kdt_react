f68f1d59337b6a2d509ab3218452c67787cc669a
채란 <nemo20435@gmail.com>
2023-01-20 11:10:00 +0900

    Update App.js

--- kdt_app/src/App.js
+++ kdt_app/src/App.js
@@ -1,45 +1,17 @@
-import { useState } from "react";
-import Lottie from 'react-lottie';
-
-import * as animationData from './lego.json';
-import Test from "./Test";
+import Footer from "./components/layouts/Footer";
+import Header from "./components/layouts/Header";
+import Main from "./components/pages/Main";
 
 function App() {
 
-  const [number, setNumber] = useState(0);
-
-  const defaultOptions = {
-    loop: true,
-    autoplay: true, 
-    animationData: animationData,
-    rendererSettings: {
-      preserveAspectRatio: 'xMidYMid slice'
-    }
-  };
-
-  const increment = () => {
-    setNumber(number + 1);
-    console.log(number);
-  };
-
-  const decrement = () => {
-    setNumber(number - 1);
-    console.log(number);
-  };
-
   return (
-    <div>
-      <div>
-        <Lottie options={defaultOptions}
-          height={400}
-          width={400}
-        />
+    <>
+      <div id="wrap">
+        <Header/>
+        <Main/>
+        <Footer/>
       </div>
-      <h1>{number}</h1>
-      <button onClick={increment}>Increment</button>
-      <button onClick={decrement}>Decrement</button>
-      <Test text = {number}/>
-    </div>
+    </>
   );
 }
 
