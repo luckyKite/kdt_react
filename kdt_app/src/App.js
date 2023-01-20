e46fd4c2817ea8ff619e8090392bea11b54af134
채란 <nemo20435@gmail.com>
2023-01-20 16:09:54 +0900

    true, false 여부로 화면에 출력하기

--- kdt_app/src/App.js
+++ kdt_app/src/App.js
@@ -6,11 +6,11 @@ import Main from "./pages/Main"
 function App() {
 
   const fruit = [
-    { id: 1, name: '사과', quantity: 2 },
-    { id: 2, name: '바나나', quantity: 3},
-    { id: 3, name: '포도', quantity: 5},
-    { id: 4, name: '배', quantity: 10},
-    { id: 5, name: '호박', quantity: 1}
+    { id: 1, name: '사과', quantity: 2, isView: 1 },
+    { id: 2, name: '바나나', quantity: 3, isView: 0},
+    { id: 3, name: '포도', quantity: 5, isView: 1},
+    { id: 4, name: '배', quantity: 10, isView: 1},
+    { id: 5, name: '호박', quantity: 1, isView: 0}
   ];
 
   return (
@@ -22,6 +22,7 @@ function App() {
           key={item.id}
           name={item.name}
           quantity={item.quantity}
+          isView={item.isView}
         />
         )
       )
--- kdt_app/src/FruitPage.js
+++ kdt_app/src/FruitPage.js
@@ -1,13 +1,18 @@
 import React from "react";
 
-function FruitPage({name, quantity}) {
+function FruitPage({name, quantity, isView}) {
 
   console.log(name, quantity);
   return (
     <div>
-      <h2>과일이름 : {name}</h2>
-      <h3>개수 : {quantity}</h3>
-
+      {
+        isView ?
+        <>
+        <h2>과일이름 : {name}</h2>
+        <h3>개수 : {quantity}</h3>
+        </>
+        : ""
+      }
     </div>
 
   );
