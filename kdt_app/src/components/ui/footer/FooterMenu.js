627c62d694ca21bac76b65f189d55290a773e611
채란 <nemo20435@gmail.com>
2023-01-20 15:15:00 +0900

    Update FooterMenu.js

--- kdt_app/src/components/ui/footer/FooterMenu.js
+++ kdt_app/src/components/ui/footer/FooterMenu.js
@@ -1,15 +1,17 @@
 import React from 'react';
 
-function FooterMenu() {
+function FooterMenu({item}) {
+
+  console.log(item);
+
   return ( 
     <ul>
-      <li className="footer_menu_ttl"><a className="en" href="#;">COMPANY<span className="footer_arrow_down"></span></a></li>
-      <li><a href="/footer/company/index.do">한눈에 보기</a></li>
-      <li><a href="/footer/company/mission.do">스타벅스 사명</a></li>
-      <li className="footer_2depth_ttl"><a href="#">스타벅스 소개<span className="footer_arrow_down"></span></a></li>
-      <li><a href="/footer/company/news_list.do">국내 뉴스룸</a></li>
-      <li><a href="/footer/company/global_starbucks.do">세계의 스타벅스</a></li>
-      <li><a href="https://news.starbucks.com" target="_blank">글로벌 뉴스룸</a></li>
+      <li className="footer_menu_ttl"><a className="en" href={item.link}>{item.name}<span className="footer_arrow_down"></span></a></li>
+      {
+        item.subMenu.map( subItem => (
+          <li key={subItem.id}><a href={subItem.link}>{subItem.name}</a></li>
+        ))
+      }
     </ul>
   );
 }
