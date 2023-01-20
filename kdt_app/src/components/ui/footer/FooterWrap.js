ccb8ec2bfdaebaad80133802f560647d81f73de7
채란 <nemo20435@gmail.com>
2023-01-20 15:15:03 +0900

    Update FooterWrap.js

--- kdt_app/src/components/ui/footer/FooterWrap.js
+++ kdt_app/src/components/ui/footer/FooterWrap.js
@@ -9,16 +9,17 @@ function FooterWrap() {
   return ( 
 
     <div className="footer_wrap ">
-					<div className="footer_menus">
-						{
-							footerMenuDatas.map(item => (
-								<FooterMenu 
-									key={item.id}
-								/>
-							))
-						}
-					</div>
-				</div>
+			<div className="footer_menus">
+				{
+					footerMenuDatas.map(item => (
+						<FooterMenu 
+							key={item.id}
+							item={item}
+						/>
+					))
+				}
+			</div>
+		</div>
    );
 }
 
