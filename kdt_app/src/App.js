2f247e81dd698e303f4d08166763f040dcad30c7
채란 <nemo20435@gmail.com>
2023-01-22 13:57:48 +0900

    Update App.js

--- kdt_app/src/App.js
+++ kdt_app/src/App.js
@@ -1,20 +1,14 @@
-import Sample from "./Sample";
-import React, { useEffect, useState } from 'react';
-
+import Footer from "./components/layouts/Footer";
+import Header from "./components/layouts/Header";
+import Main from "./pages/Main";
 
 function App() {
 
-  const [카드명세, 아빠카드] = useState("")
-  useEffect( () => {
-    아빠카드("카드 사용 준비가 되었습니다.")
-  },[]);
-
   return (
     <>
-      <Sample 
-      카드명세 = {카드명세}
-      아빠카드 = {아빠카드}/>
-      <h2>아빠 핸드폰 : {카드명세}</h2>
+     <Header/>
+     <Main />
+     <Footer />
     </>
   );
 }
