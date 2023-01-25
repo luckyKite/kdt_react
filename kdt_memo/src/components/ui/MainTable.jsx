464f2108be53646218648d6418b93d764dfc0d0d
채란 <nemo20435@gmail.com>
2023-01-25 10:49:33 +0900

    체크박스, 버튼생성

--- kdt_memo/src/components/ui/MainTable.jsx
+++ kdt_memo/src/components/ui/MainTable.jsx
@@ -1,33 +1,34 @@
 import Table from 'react-bootstrap/Table';
+import Forms from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+
 
 function MainTable() {
   return (
     <Table striped bordered hover>
       <thead>
         <tr>
-          <th>#</th>
-          <th>First Name</th>
-          <th>Last Name</th>
-          <th>Username</th>
+          <th>id</th>
+          <th>check</th>
+          <th>english</th>
+          <th>korean</th>
+          <th>controller</th>
         </tr>
       </thead>
       <tbody>
         <tr>
           <td>1</td>
-          <td>Mark</td>
-          <td>Otto</td>
-          <td>@mdo</td>
-        </tr>
-        <tr>
-          <td>2</td>
-          <td>Jacob</td>
-          <td>Thornton</td>
-          <td>@fat</td>
-        </tr>
-        <tr>
-          <td>3</td>
-          <td colSpan={2}>Larry the Bird</td>
-          <td>@twitter</td>
+          <td>
+            <Forms>
+              <Forms.Check type="checkbox" />
+            </Forms>
+          </td>
+          <td>apple</td>
+          <td>사과</td>
+          <td>
+            <Button variant="primary">답 보기</Button>
+            <Button variant="secondary">삭제</Button>
+          </td>
         </tr>
       </tbody>
     </Table>
