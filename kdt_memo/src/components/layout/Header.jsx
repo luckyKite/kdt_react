3f90d0dd5e9be599812e83c14aa5742da89c5042
채란 <nemo20435@gmail.com>
2023-01-25 10:31:15 +0900

    Update Header.jsx

--- kdt_memo/src/components/layout/Header.jsx
+++ kdt_memo/src/components/layout/Header.jsx
@@ -7,23 +7,12 @@ function Header() {
   return (
     <Navbar bg="light" expand="lg">
       <Container>
-        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+        <Navbar.Brand>KDT_MEMO</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#link">Link</Nav.Link>
-            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">
-                Another action
-              </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">
-                Separated link
-              </NavDropdown.Item>
-            </NavDropdown>
+            <Nav.Link>단어추가 </Nav.Link>
+            <Nav.Link>DAY 추가</Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
