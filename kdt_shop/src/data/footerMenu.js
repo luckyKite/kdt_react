ddf25ddd2a0f2eea5d86f8c08609ea3631c6e32c
채란 <nemo20435@gmail.com>
2023-01-26 14:14:18 +0900

    footer 정리

---
+++ kdt_shop/src/data/footerMenu.js
@@ -0,0 +1,32 @@
+export const footerMenu = [
+  {
+    id: 1,
+    name: '카테고리',
+    link: '/category',
+    icon: 'category.svg'
+  },
+  {
+    id: 2,
+    name: '선물하기',
+    link: '/present',
+    icon: 'present.svg'
+  },
+  {
+    id: 3,
+    name: '홈',
+    link: '/',
+    icon: 'home.svg'
+  },
+  {
+    id: 4,
+    name: 'MY',
+    link: '/my',
+    icon: 'my.svg'
+  },
+   {
+    id: 5,
+    name: '최근본',
+    link: '/recently',
+    icon: 'recently.svg'
+  }
+]
