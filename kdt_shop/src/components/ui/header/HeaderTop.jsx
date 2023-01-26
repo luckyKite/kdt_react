52b001c73c444b3461a826f6285055cae68afe2b
채란 <nemo20435@gmail.com>
2023-01-26 11:35:22 +0900

    아이콘 추가, 검색기능

--- kdt_shop/src/components/ui/header/HeaderTop.jsx
+++ kdt_shop/src/components/ui/header/HeaderTop.jsx
@@ -1,18 +1,48 @@
-import React from 'react';
-import style from './HeaderTop.module.css'
+import React, { useState } from 'react';
+import Lottie from 'react-lottie';
+import style from './HeaderTop.module.css';
+import * as animationData from '../../../lottie/Cart.json';
 
 function HeaderTop() {
-  return (
+
+  const defaultOptions = {
+    loop: true,
+    autoplay: true, 
+    animationData: animationData,
+    rendererSettings: {
+      preserveAspectRatio: 'xMidYMid slice'
+    }
+  };
+
+  const [searchWord, setSearchWord] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+  }
+
+  const handleChange = (e) => {
+    console.log(e.target.value);
+    setSearchWord(e.target.value);
+  }
+
+  return ( 
     <div className={style.headerTopWrap}>
       <h1 className={style.logo}>SSG.COM</h1>
-      <div className={style.search}>
-        <input type="text" />
-      </div>
+      <form className={style.search} onSubmit={handleSubmit}>
+        <input type="text" 
+          onChange={handleChange}
+          defaultValue={searchWord}
+        />
+      </form>
       <div className='cartIcon'>
-        장바구니 아이콘
+        <Lottie options={defaultOptions}
+          height={60}
+          width={60}
+        />
       </div>
     </div>
-    );
+   );
 }
 
 export default HeaderTop;
