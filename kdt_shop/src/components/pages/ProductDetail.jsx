7c4f2d8c46a37f8a49d06d7b829151951606e746
채란 <nemo20435@gmail.com>
2023-01-26 15:11:36 +0900

    상세보기 화면페이지

---
+++ kdt_shop/src/components/pages/ProductDetail.jsx
@@ -0,0 +1,49 @@
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+
+function ProductDetail() {
+  const { id } = useParams();
+  const [product, setProduct] = useState();
+
+  useEffect( () => {
+    fetch(`https://dummyjson.com/products/${id}`)
+    .then(res => res.json())
+    .then(data => {
+      console.log(data);
+      setProduct(data);
+    })
+    .catch(err => console.log(err))
+  },[id]);
+
+  return (
+    <div>
+      {
+        product && (
+          <>
+            <img src={product.thumbnail} alt={product.description} />
+            <h2>{product.title}</h2>
+            <p>{product.description}</p>
+            <p>{product.price}</p>
+            <p>{product.discountPercentage}</p>
+            <p>{product.price - (product.price * product.discountPercentage)}</p>
+            <p>{product.rating}</p>
+            <p>{product.stock}</p>
+            <p>{product.brand}</p>
+            <p>{product.category}</p>
+            {
+              product.images.map( (image, idx) => (
+                <img 
+                  src={image} 
+                  alt={`${product.description}+${idx}`} 
+                  key={idx}
+                />
+              ))
+            }
+          </>
+        )
+      }
+    </div>  
+  );
+}
+
+export default ProductDetail;
