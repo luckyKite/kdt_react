e71a4c4a817fe06f3f39ddfa4e337b61e8f5cdf5
채란 <nemo20435@gmail.com>
2023-01-26 10:51:15 +0900

    메뉴 db로 관리하기

---
+++ kdt_shop/src/data/gnbMenu.js
@@ -0,0 +1,32 @@
+export const gnbMenu = [
+  {
+    id: 1,
+    name: '홈',
+    link: '/'
+  },
+  {
+    id: 2,
+    name: '특가',
+    link: '/special'
+  },
+  {
+    id: 3,
+    name: '랭킹',
+    link: '/ranking'
+  },
+  {
+    id: 4,
+    name: 'SSG.TV',
+    link: '/tv'
+  },
+  {
+    id: 5,
+    name: '명품관',
+    link: '/luxury',
+  },
+  {
+    id: 6,
+    name: '브랜드',
+    link: '/brand'
+  }
+];
