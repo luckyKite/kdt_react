1605512aebdbb8d517e9faaed8d69e038f066cad
채란 <nemo20435@gmail.com>
2023-01-26 16:09:28 +0900

    버튼 누르면 장바구니 담는 기능

--- kdt_shop/src/components/ui/main/ProductCard.jsx
+++ kdt_shop/src/components/ui/main/ProductCard.jsx
@@ -1,8 +1,33 @@
 import React from 'react';
 import style from './ProductCard.module.css';
-import { Link } from 'react-router-dom';
+import { isRouteErrorResponse, Link } from 'react-router-dom';
 
 function productCard({product}) {
+  
+  const userId = 1;
+
+  const handleAddCart = () => {
+    fetch('https://dummyjson.com/carts/add', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({
+        userId: userId,
+        products: [
+          {
+            id: product.id,
+            quantity: 1,
+          }
+        ]
+      })
+    })
+    .then(res => {
+      res.json();
+      if(res.ok) {
+        window.alert('장바구니 담기');
+      }
+    })
+    .catch(err => console.error(err));
+  }
 
   return ( 
     <>
@@ -13,7 +38,7 @@ function productCard({product}) {
           <p>{product.description}</p>
           <p>{product.price}</p>
           </Link>
-          <div className={style.cartBtn}>ADD CART</div>
+          <div className={style.cartBtn} onClick={handleAddCart}>ADD CART</div>
       </div>
     </>
    );
