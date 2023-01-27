050fe2adb3cd49098012b5b23219106e5b6ba14d
채란 <nemo20435@gmail.com>
2023-01-27 17:26:54 +0900

    header css 정리하는중

--- starbucks/src/components/layout/Header.jsx
+++ starbucks/src/components/layout/Header.jsx
@@ -1,14 +1,19 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import logo from '../../images/logo/logo.png';
+import style from './Header.module.css';
 
 function Header() {
   return (
-    <div className='header'>
-      <div className='logo'><img src={logo} alt="로고이미지" /></div>
-      <div className='menu'>Menu</div>
-      <div className='cart'>Cart</div>
-      <div className='story'>Story</div>
+    <div className={style.header}>
+      <div className={style.logo}><img src={logo} alt="로고이미지" /></div>
+      <nav className={style.nav}>
+        <ul>
+          <li className={style.menu}>Menu</li>
+          <li className={style.cart}>Cart</li>
+          <li className={style.story}>Story</li>
+        </ul>
+      </nav>
     </div>
 
   );
