4692b7464293c1d8b37bcf35ff4fb656d65f8978
채란 <nemo20435@gmail.com>
2023-01-27 09:25:57 +0900

    cart page

--- kdt_shop/src/components/pages/Cart.jsx
+++ kdt_shop/src/components/pages/Cart.jsx
@@ -15,8 +15,24 @@ function Cart() {
 
 
   return (
-    <>
-    </>
+    <div>
+      <div className='cartListCard'>
+        <img src="" alt="" />
+        <div>
+          <h3>상품명</h3>
+          <p>price</p>
+          <div className='qtyHandler'>
+            <button></button>
+            <p>1</p>
+            <button>+</button>
+          </div>
+          <p>total price</p>
+        </div>
+        <div>
+          <button>삭제</button>
+        </div>
+      </div>
+    </div>
   );
 }
 
