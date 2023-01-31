4c888a0b51bc4ca52a2e330a166705ce170f70d5
채란 <nemo20435@gmail.com>
2023-01-31 22:01:10 +0900

    카트담기 수정

--- starbucks/src/components/pages/Menu.jsx
+++ starbucks/src/components/pages/Menu.jsx
@@ -1,12 +1,30 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import MenuList from '../ui/menu/MenuList';
 import style from './Menu.module.css';
 
 function Menu() {
+  const [menu, setMenu] = useState();
+
+  useEffect(() => {
+    fetch(`http://localhost:3001/menu`)
+      .then(res => res.json())
+      .then(data => {
+        setMenu(data);
+      })
+  }, []);
+
   return (
     <div className={style.drink}>
       <h1>음료</h1>
-      <MenuList />
+      <div className={style.choice}>
+        <ul className={style.menuList}>
+          {
+            menu && menu.map((item, idx) =>
+              <MenuList menu={item} key={idx} />
+            )
+          }
+        </ul>
+      </div>
     </div>
   );
 }
--- starbucks/src/components/ui/menu/MenuList.jsx
+++ starbucks/src/components/ui/menu/MenuList.jsx
@@ -1,60 +1,40 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import style from './MenuList.module.css';
-import menus from '../../../db/data.json';
 import { Link } from 'react-router-dom';
 
-function MenuList() {
-
-  const [menu, setMenu] = useState();
+function MenuList({ menu }) {
 
   const handleAddCart = () => {
     fetch('http://localhost:3001/cart', {
       method: 'POST',
-      headers: { 'Content-Type' : 'application/json'},
+      headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({
         menuId: menu.id,
         userId: 1,
-        qty:1,
+        qty: 1,
       })
     })
-    .then(res => {
-      res.json();
-      if(res.ok) {
-        window.alert(`카트에 추가합니다.`);
-      }
-    })
+      .then(res => {
+        res.json();
+        if (res.ok) {
+          window.alert(`카트에 추가합니다.`);
+        }
+      })
   }
 
-  useEffect( () => {
-    fetch(`http://localhost:3001/menu`)
-    .then(res => res.json())
-    .then(data => {
-      setMenu(data);
-    })
-  },[]);
-
   return (
-    <div className={style.choice}>
-      <ul className={style.menuList}>
-        {
-          menu && menu.map( menu =>
-          <li key={menu.id}>
-            <Link to={`/menu-detail/${menu.id}`}>
-              <img src={menu.images} alt="음료사진"/>
-            </Link>
-            <div>{menu.title}</div>
-            <button 
-              className={style.btn} 
-              onClick={handleAddCart}
-            >
-              카트에 담기
-            </button>
-          </li>
-          )
-        }
-      </ul>
-    </div>
-  
+    <li key={menu.id}>
+      <Link to={`/menu-detail/${menu.id}`}>
+        <img src={menu.images} alt="음료사진" />
+      </Link>
+      <div>{menu.title}</div>
+      <button
+        className={style.btn}
+        onClick={handleAddCart}
+      >
+        카트에 담기
+      </button>
+    </li>
   );
 }
 
