8bc50e6ca367ff7692b865953f0952faaeb4a6a8
채란 <nemo20435@gmail.com>
2023-01-31 10:27:12 +0900

    useMemo 사용 예제

--- test_app/src/App.js
+++ test_app/src/App.js
@@ -1,24 +1,45 @@
+import React, { useState, useEffect, useMemo } from 'react';
 import './App.css';
-import { useEffect, useState } from 'react';
-import Header from './components/layout/Header';
-import Main from './components/pages/Main';
-import Login from './components/pages/Login';
 
-import { RecoilRoot, useRecoilValue } from 'recoil';
-import { loginState } from './components/state/LoginState';
+
+const bigProcess = (number) => {  
+  let sum = 0;
+  for (let i = 0; i < 1000000000; i++) {
+    sum += i;
+  }
+  console.log('bigProcess');
+  return 100000 + number;
+}
+
+const smallProcess = (number) => {
+  let sum = 0 ;
+  for (let i = 0; i < 10; i++) {
+    sum += i;
+  }
+  console.log('smallProcess');
+  return number;
+}
 
 function App() {
 
-  const isLogin = useRecoilValue(loginState);
+  const [number, setNumber] = useState(0);
+  const [bigNumber, setBigNumber] = useState(0);
+
+  // const bigSum = bigProcess(bigNumber);
+  const bigSum = useMemo(()=>{
+    return bigProcess(bigNumber);
+  },[bigNumber]);
+  const smallSum = smallProcess(number);
+
 
   return (
-    <div className='App'>
-      <Header />
-      {
-        isLogin ? <Main /> : <Login />
-      }     
-      
-    </div>
+      <div className="App">
+        <h2>BigProcess: {bigSum}</h2>
+        <h2>SmallProcess: {smallSum}</h2>
+        <button onClick={()=>setBigNumber(bigNumber+1)}>bigProcess</button>
+        <button onClick={()=>setNumber(number+1)}>smallProcess</button>
+      </div>
+    
   );
 }
 
